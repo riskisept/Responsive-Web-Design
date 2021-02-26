b126e83e3e857a847068dce41cce6fd83ede8c68
I Kadek Riski Septiawan 1915101049 <79486784+riskisept@users.noreply.github.com>
2021-02-26 18:07:31 +0800

    Add files via upload

---
+++ riskiweb/index.php
@@ -0,0 +1,176 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
+  <title>Genshin Impact</title>
+</head>
+
+<body>
+  <section class="header">
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <a class="navbar-brand" href="#"><img src="assets/a2.png"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <i class="fa fa-bars" aria-hidden="true"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav text-right ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link active-home" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Character</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Weapons</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Farming Guide</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </section>
+
+  <section class="slide">
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="assets/w6-1.jpg" alt="First slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="assets/w6-1.jpg" alt="First slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="assets/w6-1.jpg" alt="Third slide">
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </section>
+  <section class="text">
+    <div class="container">
+      <h1 class="tittle">New Event</h1>
+      <p>L Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi, voluptate vel? Consequuntur maxime debitis molestiae incidunt? Impedit placeat totam laudantium soluta sint alias aliquam maxime enim inventore odit, consectetur sed!</p>
+
+      <div class="row ">
+        <div class="col-md-4">
+          <div class="galery-box">
+            <img src="assets/a3-1.png">
+            <h6>Content 1</h6>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur pariatur architecto provident omnis at, officia maiores hic. Facere.</p>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="galery-box">
+            <img src="assets/a4.png">
+            <h6>Content 2</h6>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur pariatur architecto provident omnis at, officia maiores hic. Facere.</p>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="galery-box">
+            <img src="assets/a5.png">
+            <h6>Content 3</h6>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur pariatur architecto provident omnis at, officia maiores hic. Facere.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <section id="contact" class="contact-mg-5">
+    <div class="container">
+      <div class="row pt-4 mb-4">
+        <div class="col text-corner">
+          <h2>Contact Me</h2>
+        </div>
+      </div>
+
+      <div class="row justify-content">
+        <div class="col-lg-4">
+          <div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">@genshininfo</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+          <ul class="list-group">
+            <li class="list-group-item">
+              <h1>Location</h1>
+            </li>
+            <li class="list-group-item">My Office</li>
+            <li class="list-group-item">Jln. Seririt-Pupuan</li>
+            <li class="list-group-item">Bali, Indonesia</li>
+          </ul>
+        </div>
+        <div class="col-lg-6">
+          <form>
+            <div class="form-group">
+              <label for="Nama">Nama</label>
+              <input type="text" class="form-control" id="Nama">
+            </div>
+
+            <div class="form-group">
+              <label for="Email">Email</label>
+              <input type="text" class="form-control" id="Email">
+            </div>
+
+            <div class="form-group">
+              <label for="Nama">No Telepon</label>
+              <input type="text" class="form-control" id="Nama">
+            </div>
+
+            <div class="form-group">
+              <label for="Nama">Pesan</label>
+              <input type="text" class="form-control" id="Nama">
+            </div>
+
+            <div class="form-group">
+              <button type="button" class="btn btn-primary">Kirim</button>
+            </div>
+          </form>
+
+        </div>
+      </div>
+  </section>
+
+  <section>
+    <footer class="bg-dark text-white">
+    <div class="container">
+      <div class="row text-center pt-3">
+        <div class="col">
+          <p>Copyright 2021</p>
+        </div>
+      </div>
+    </div>
+    </footer>
+  </section>
+</body>
+
+</html>
